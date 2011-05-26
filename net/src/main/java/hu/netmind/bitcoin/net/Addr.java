2bcbe495df14825b72ae62c724ce9fb2f4e64507
Robert Brautigam <robert.brautigam@netmind.hu>
2011-05-26 20:00:21 +0000

    Added Addr message

---
+++ net/src/main/java/hu/netmind/bitcoin/net/Addr.java
@@ -0,0 +1,47 @@
+/**
+ * Copyright (C) 2011 NetMind Consulting Bt.
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 3 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ */
+
+package hu.netmind.bitcoin.net;
+
+import java.util.List;
+
+/**
+ * @author Robert Brautigam
+ */
+public interface Addr extends Message
+{
+   /**
+    * Get the address entries in this message.
+    */
+   List<AddressEntry> getAddressEntries();
+
+   interface AddressEntry
+   {
+      /**
+       * Get the timestamp. (What is this exactly used for?) Note:
+       * only from protocol version 31402.
+       */
+      long getTimestamp();
+
+      /**
+       * Get the actual address.
+       */
+      NodeAddress getAddress();
+   }
+}
+
