ae61c051300f1e9c4f623940c012f22e7b0de5fa
Robert Brautigam <robert.brautigam@netmind.hu>
2011-06-16 17:07:20 +0000

    Renamed ec to ecc

--- keystore-ecc/src/main/java/hu/netmind/bitcoin/keystore/ecc/KeyStoreImpl.java
+++ keystore-ecc/src/main/java/hu/netmind/bitcoin/keystore/ecc/KeyStoreImpl.java
@@ -16,7 +16,7 @@
  * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
  */
 
-package hu.netmind.bitcoin.keystore.ec;
+package hu.netmind.bitcoin.keystore.ecc;
 
 import hu.netmind.bitcoin.KeyStore;
 import hu.netmind.bitcoin.Key;
--- keystore-ecc/src/main/java/hu/netmind/bitcoin/keystore/ecc/Storage.java
+++ keystore-ecc/src/main/java/hu/netmind/bitcoin/keystore/ecc/Storage.java
@@ -16,7 +16,7 @@
  * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
  */
 
-package hu.netmind.bitcoin.keystore.ec;
+package hu.netmind.bitcoin.keystore.ecc;
 
 /**
  * A storage mechanism for storing and retrieving keys.
