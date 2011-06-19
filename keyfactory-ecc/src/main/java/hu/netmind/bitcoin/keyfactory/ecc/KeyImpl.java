1ec90436f97ad96de0d3d96525bbe5ca1cddd03b
Robert Brautigam <robert.brautigam@netmind.hu>
2011-06-19 11:15:58 +0000

    Refactored classes into keyfactory package

--- keyfactory-ecc/src/main/java/hu/netmind/bitcoin/keyfactory/ecc/KeyImpl.java
+++ keyfactory-ecc/src/main/java/hu/netmind/bitcoin/keyfactory/ecc/KeyImpl.java
@@ -16,7 +16,7 @@
  * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
  */
 
-package hu.netmind.bitcoin.keystore.ecc;
+package hu.netmind.bitcoin.keyfactory.ecc;
 
 import hu.netmind.bitcoin.Key;
 import hu.netmind.bitcoin.PublicKey;
