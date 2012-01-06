7544513a5998563d88286eac56be484db95e75f2
Robert Brautigam <robert.brautigam@netmind.hu>
2012-01-06 18:09:08 +0000

    Fixed some API comment details

--- core/block/src/main/java/hu/netmind/bitcoin/block/BlockChainLinkStorage.java
+++ core/block/src/main/java/hu/netmind/bitcoin/block/BlockChainLinkStorage.java
@@ -30,44 +30,50 @@ public interface BlockChainLinkStorage
    /**
     * Get the very first link in the storage, which should contain the genesis
     * block of this chain. This might be null if there are no
-    * blocks yet in the storage.
+    * blocks yet in the storage. Orphan blocks are never returned.
     */
    BlockChainLink getGenesisLink();
 
    /**
     * Get the link stored which has the greatest aggregated complexity
-    * and is connected to the genesis block.
+    * and is connected to the genesis block. Orphan blocks are never returned.
     */
    BlockChainLink getLastLink();
 
    /**
-    * Get the link to the given hash value.
+    * Get the link to the given hash value. Orphan blocks are returned.
     * @return The link with the block with the given hash, or null if no such link exists.
     */
    BlockChainLink getLink(byte[] hash);
 
    /**
     * Get the links for which the previous link contains the block with hash
-    * given.
+    * given. Orphan blocks are returned.
     */
    List<BlockChainLink> getNextLinks(byte[] hash);
 
    /**
-    * Find the link with the block which contains the claimed output by the input given.
-    * All the links with blocks including the given one and all parents
-    * of it will be checked (this link denotes the branch to search).
-    * @param link The link with the block that represents the branch to search.
-    * @param in The tansaction input claiming the output to find the block for.
-    * @return The link that contains the block that contains the said transaction.
+    * Find the link which contains the claimed transaction by the input given.
+    * All the links including the given one and all parents
+    * of it will be checked (the link denotes the branch to search). Note that
+    * only the transaction hash should be checked, the output index is not
+    * relevant for this search.
+    * @param link The link that represents the top of the branch to search. If
+    * this is an orphan link, the result is undefined.
+    * @param in The tansaction input claiming the output to find the link for.
+    * @return The link that contains the claimed output, or null if no such
+    * link exist.
     */
    BlockChainLink getClaimedLink(BlockChainLink link, TransactionInput in);
 
    /**
-    * Find the link with the block which contains a claim for the same transaction output
+    * Find the link which contains a claim for the same transaction output
     * that is claimed by the given input.
     * @param link The link with the block that represents the branch to search.
-    * @param in The tansaction input claiming the output to find the block for.
-    * @return The block that contains the said input if any.
+    * Supplying an orphan link yields an undefined result.
+    * @param in The tansaction input claiming the output to find the link for.
+    * @return The link that contains the said input, or null if the same output
+    * is not claimed in the given branch.
     */
    BlockChainLink getClaimerLink(BlockChainLink link, TransactionInput in);
 
